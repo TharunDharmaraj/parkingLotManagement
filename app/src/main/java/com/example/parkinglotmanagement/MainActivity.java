b00035478c2c9236071832a0de53bf81f574ce93
tharun <tharundharmaraj@gmail.com>
2022-08-05 03:19:30 +0530

    Themes set

--- app/src/main/java/com/example/parkinglotmanagement/MainActivity.java
+++ app/src/main/java/com/example/parkinglotmanagement/MainActivity.java
@@ -1,27 +1,21 @@
 package com.example.parkinglotmanagement;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    boolean doubleBackToExitPressedOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.doubleBackToExitPressedOnce = true;
         new Handler(Looper.getMainLooper()) {
-
-        }.postDelayed(new Runnable() {
-
-            @Override
-            public void run() {
-                doubleBackToExitPressedOnce = false;
-            }
+        }.postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, login.class));
         }, 2000);
     }
 }
